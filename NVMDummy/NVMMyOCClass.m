e116397aedbaba3693a7d6a2dc727d41a20f9b5d
Bryan Boyko <bryan@maxprebid.com>
2018-02-14 14:09:41 -0800

    Remove Swift references in ObjC class

--- NVMDummy/NVMMyOCClass.m
+++ NVMDummy/NVMMyOCClass.m
@@ -7,16 +7,8 @@
 //
 
 #import "NVMMyOCClass.h"
-#import <NVMDummy/NVMDummy-Swift.h>
 
 @implementation NVMMyOCClass
-
-+ (void)describe {
-  NSLog(@"++++This is objc code calling swift func++++");
-  NSLog(@"%@", [[MySWClass new] description]);
-  NSLog(@"+++++++++++");
-  
-}
 
 - (NSString *)description {
   return @"This is description of an objc class";
